b3abac7d184239c5bab88b014a6146acb20cf940
Goeun1027 <jeeny200@gmail.com>
2022-08-04 05:13:26 +0000

    test

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -38,7 +38,7 @@ export default function Layout() {
                 <HomeIcon />
               </IconButton>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                ExamApp for T2
+                ExamApp for T2!
               </Typography>
             </Toolbar>
           </AppBar>
